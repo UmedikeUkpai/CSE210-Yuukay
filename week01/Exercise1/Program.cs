65994f49c31d986e1626febb61ba5941f2243e3d
UmedikeUkpai <uumedike@byupathway.edu>
2026-03-10 03:07:51 -0600

    Added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        // This is the main entry point of the program. It will execute when the program is run.
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
     }
 }
